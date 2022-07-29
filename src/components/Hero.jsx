db79a93c0283990323684c25d21e0387a29dc266
Bilen <raybimlendra@gmail.com>
2022-07-29 08:53:28 +0545

    modified:   src/App.css 	modified:   src/components/Hero.css 	modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import "./Hero.css"
 import Header from "./Header/Header"
 
+import hero_image from "../assets/hero_image.png"
+import hero_image_back from "../assets/hero_image_back.png"
+import heart from "../assets/heart.png"
+import Calories from "../assets/calories.png"
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -27,8 +32,50 @@ const Hero = () => {
             <span>In here you can do the gym work and everything.</span>
           </div>
         </div>
+
+        {/*figures */}
+        <div className='figures'>
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/*hero button */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get started</buttons>
+          <buttons className="btn">Learn more</buttons>
+        </div>
       </div>
-      <div className="right">Right Side</div>
+      <div className="right">
+        <button className="btn">
+          Join Now
+        </button>
+        <div className='heart-rate'>
+          <img src={heart} alt="heart"/>
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        {/* hero image */}
+        <img src={hero_image} alt="hero_image" className='hero_image'/>
+        <img src= {hero_image_back} alt="back_image" className='hero_image_back'/>
+
+        {/* calories */}
+        <div className='calories'>
+          <img src = {Calories} alt = "calories"/>
+          <div>
+            <span>Calories burned</span>
+            <span>220 Kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
