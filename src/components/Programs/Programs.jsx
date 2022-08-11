8752e8e33f8ce64d6976fdc9b7355d4559a1f318
Bilen <raybimlendra@gmail.com>
2022-08-12 01:05:57 +0545

    new file:   src/components/Programs/Programs.css 	new file:   src/components/Programs/Programs.jsx

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Programs.css";
+
+function Programs() {
+  return (
+    <div className="Programs">
+        <div className="programs-header">
+            <span>Explore our</span>
+            <span>Programs</span>
+            <span>to shape you</span>
+        </div>
+    </div>
+  )
+}
+
+export default Programs
